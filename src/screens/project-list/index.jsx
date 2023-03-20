7c63e59d684e8f325f2bbd5d9ab3d222fd62592e
lulu <2311013292@qq.com>
2023-03-20 22:49:44 +0800

    3-2process.env环境变量

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { SearchPanel } from "./search-panel"
 import { List } from "./list"
 
+// process.env动态获取环境变量 
+const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
     name: '',
@@ -12,7 +14,7 @@ export const ProjectListScreen = () => {
   // 当param改变时(用户输入关键词，或选择下拉框时)，获取列表接口
   useEffect(() => {
     // fetch 返回的是一个 Promise 对象
-    fetch('http://localhost:3001/projects').then(async response => {
+    fetch(`${apiUrl}/projects`).then(async response => {
       if (response.ok) { // 如请求成功，response.ok 的值为 true
         setList(await response.json());
       }

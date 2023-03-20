5064c0872a1020e2e1b3a03d45c876e3b7135eed
lulu <2311013292@qq.com>
2023-03-20 17:32:57 +0800

    3-2状态提升

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,3 +1,28 @@
+import { useState, useEffect } from "react";
+import { SearchPanel } from "./search-panel"
+import { List } from "./list"
+
 export const ProjectListScreen = () => {
-  return <div></div>
+  const [param, setParam] = useState({
+    name: '',
+    personId: ''
+  })
+  const [list, setList] = useState([]);
+
+  // 当param改变时(用户输入关键词，或选择下拉框时)，获取列表接口
+  useEffect(() => {
+    // fetch 返回的是一个 Promise 对象
+    fetch('http://localhost:3001/projects').then(async response => {
+      if (response.ok) { // 如请求成功，response.ok 的值为 true
+        setList(await response.json());
+      }
+    })
+  }, [param])
+
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} />
+      <List list={list} />
+    </div>
+  )
 }

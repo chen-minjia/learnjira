a1a6fc340dd21ee2e4a8bc4f9977f4eb58ecbb0c
lulu <2311013292@qq.com>
2023-03-26 12:12:44 +0800

    给useDebounce添加泛型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -38,7 +38,7 @@ export const useMount = (callback: () => void) => {
 // }
 
 // 防抖 
-export const useDebounce = (value: unknown, delay?: number): any => {
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     // 每次 value 变化，设置一个定时器
